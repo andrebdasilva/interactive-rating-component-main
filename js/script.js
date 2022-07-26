5ec855243ad0415729365e096c19ca588a0f83d7
André Barbosa <andrebdasilva@gmail.com>
2022-07-26 09:34:19 -0300

    fix html, css, js, logic css e js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
-const listRating = document.querySelector("ul");
-const burronSubmit = document.querySelector(".btn-submit");
+const listNumbers = document.querySelector("ul");
+const numbers = document.querySelectorAll(".icon-number");
+const buttonSubmit = document.querySelector(".btn-submit");
 const containerRating = document.querySelector(".container-rating");
 const containerThankYou = document.querySelector(".container-thank-you");
 
+numbers.forEach((number) => {
+	number.addEventListener("click", handleRatingClickNumber)
+});
 
+buttonSubmit.addEventListener("click", onSubmit);
 
-listRating.addEventListener("click", function(event){
-	const liText = event.target.textContent;
-	event.target.classList.toggle("select")
+function handleRatingClickNumber(event) {
+	const classNumber = event.target.classList;
+	const numberValue = event.target.getAttribute("data-value")
 	const ratingResult = document.querySelector(".rating-result");
-	ratingResult.innerHTML = parseInt(liText);	
-});
 
-burronSubmit.addEventListener("click", function(){
+	numbers.forEach((number) => {
+		number.classList.remove("active")
+	});
+
+	if (!classNumber.contains("active")) {
+		classNumber.add("active")
+		buttonSubmit.removeAttribute("disabled")
+		ratingResult.innerHTML = numberValue;
+	};
+};
+
+function onSubmit() {
 	containerRating.style.display = "none";
-	containerThankYou.style.display = "block";	
-});
+	containerThankYou.style.display = "block";
+};
